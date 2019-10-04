391a040678eb417a2ae80dd61d0f33319b9b15f8
Mark Perkins <mark@allmarkedup.com>
2019-10-04 17:59:13 +0200

    Working on homepage

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,9 +1,14 @@
 const mix = require('laravel-mix');
+require('laravel-mix-imagemin');
 
 mix
-  .sass('site/_assets/css/main.scss', 'dist/assets/css', {
+  .sass('site/_assets/css/main.scss', 'dist/css', {
     sassOptions: {
       includePaths: ['node_modules']
     }
+  })
+  .imagemin({
+    from: 'img/*',
+    context: 'site/_assets/'
   })
   .setPublicPath('dist');
