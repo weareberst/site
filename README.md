240f54f82932bc787f082162726b16ac75ab6e77
Mark Perkins <mark@allmarkedup.com>
2019-10-24 13:25:16 +0100

    First pass at scroll animations

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 ## Berst website
 
 Static site build using [Eleventy](https://www.11ty.io).
+
+### Running locally
+
+> All commands should be run from the same directory as this README file
+
+1. Install dependencies: `npm install`
+2. Start the dev server and build assets: `npm run dev`
+3. Point your browser at http://localhost:8080 to view the site
--- site/_assets/js/main.js
+++ site/_assets/js/main.js
@@ -1,8 +1,138 @@
 const $ = require('cash-dom');
+const ScrollReveal = require('scrollreveal').default;
 
 $('.faqs__item').each(function() {
   const item = $(this);
   item.on('click', function() {
     item.toggleClass('is-open');
   });
+});
+
+function reveal(target, opts) {
+  ScrollReveal().reveal(target, opts);
+}
+
+// reveal animations
+
+ScrollReveal({
+  duration: 1000
+});
+
+ScrollReveal.debug = true;
+
+// intro
+reveal('.layout--home .masthead__logo img', {
+  delay: 250
+});
+reveal('.intro__title span:nth-child(1)', {
+  delay: 250,
+  distance: '40px',
+  origin: 'top'
+});
+reveal('.intro__title span:nth-child(2)', {
+  delay: 500,
+  distance: '40px',
+  origin: 'top'
+});
+reveal('.intro__title span:nth-child(3)', {
+  delay: 750,
+  distance: '40px',
+  origin: 'top'
+});
+reveal('.intro__title span:nth-child(4)', {
+  delay: 1000,
+  distance: '40px',
+  origin: 'top'
+});
+
+reveal('.intro__strapline', {
+  delay: 1500
+});
+reveal('.layout--home .masthead__contact', {
+  delay: 2000
+});
+
+// teaser pair
+
+reveal('.teaser-pair .teaser:first-child', {
+  distance: '100%',
+  origin: 'right',
+  delay: 250
+});
+
+reveal('.teaser-pair .teaser:last-child', {
+  distance: '100%',
+  origin: 'left'
+});
+
+// services
+
+reveal('.services__item--identify', {
+  duration: 1500
+});
+
+reveal('.services__item--develop', {
+  duration: 1500,
+  delay: 250
+});
+
+reveal('.services__item--distinguish', {
+  duration: 1500,
+  delay: 500
+});
+
+// funding
+
+reveal('.funding__panel', {
+  rotate: {
+    y: 100
+  }
+});
+
+// teaser testimonial pair
+
+reveal('.teaser-testimonial .teaser', {
+  distance: '100%',
+  origin: 'left'
+});
+
+reveal('.teaser-testimonial .testimonial');
+
+// case study content
+
+const caseStudyDefaults = {
+  distance: '50px',
+  origin: 'bottom'
+};
+
+reveal(
+  '.content--imagepair-offset .content__figure:first-child',
+  caseStudyDefaults
+);
+reveal('.content--imagepair-offset .content__figure:last-child', {
+  ...caseStudyDefaults,
+  delay: 250
+});
+
+reveal(
+  '.content--imagepair-even .content__figure:first-child',
+  caseStudyDefaults
+);
+reveal('.content--imagepair-even .content__figure:last-child', {
+  ...caseStudyDefaults,
+  delay: 250
+});
+
+reveal('.content--image-offset', caseStudyDefaults);
+reveal('.content--feature-img', caseStudyDefaults);
+reveal('.content--image-full', caseStudyDefaults);
+
+reveal('.content--text', {
+  ...caseStudyDefaults,
+  origin: 'left'
+});
+
+reveal('.content--testimonial', {
+  ...caseStudyDefaults,
+  origin: 'right'
 });
